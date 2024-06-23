f71c5b487bb2fc49bc9a399ea909a838f99cddda
Rishav Kumar <rishavkumar208@gmail.com>
2024-06-24 03:07:07 +0530

    init

---
+++ src/main/java/com/imafk/LockInApplication.java
@@ -0,0 +1,16 @@
+package com.imafk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+  LockIn is a data privacy vault.
+ */
+
+@SpringBootApplication
+public class LockInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockInApplication.class, args);
+    }
+}
